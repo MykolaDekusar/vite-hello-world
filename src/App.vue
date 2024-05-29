fb77316600ce0e79c599cb1c69d7723d7800be6b
MykolaDekusar <ndekusar97@gmail.com>
2024-05-29 15:55:27 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import AppTitle from './components/AppTitle.vue'
+
+export default {
+  name: 'Main',
+  components: {
+    AppTitle,
+  }
+}
+</script>
+
+<template>
+<AppTitle />
+</template>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: 'Title',
+    data(){
+        return{
+            title: 'La mia prima app con Vite!',
+        };
+    },
+}
+</script>
+
+<template>
+    <h1>{{ title }}</h1>
+</template>
